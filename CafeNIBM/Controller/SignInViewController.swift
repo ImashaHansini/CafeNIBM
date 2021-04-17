f383fb0ccd64ceb6f5fe0740c69c4f7b64913d69
Imasha <imasha.hansini@gmail.com>
2021-04-17 08:04:24 -0700

    Sign in and Sign Up screens done.

---
+++ CafeNIBM/Controller/SignInViewController.swift
@@ -0,0 +1,70 @@
+//
+//  SignInViewController.swift
+//  CafeNIBM
+//
+//  Created by Imasha on 4/13/21.
+//
+
+import UIKit
+import Firebase
+
+class SignInViewController: UIViewController {
+
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    
+    var number: Int?
+    var fetchedNumber: Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    @IBAction func onSignInPressed(_ sender: UIButton) {
+        if validateInput() {
+            authenticateUser(email: txtEmail.text!, password: txtPassword.text!)        }
+        else {
+            print("Input Error Found")
+        }
+    }
+    
+    func validateInput() -> Bool {
+        guard let email = txtEmail.text else {
+            print("Email is NULL")
+            return false
+        }
+        
+        guard let password = txtPassword.text else {
+                 print("Password is NULL")
+                 return false
+        }
+        
+        if email.count < 5 {
+            print("Enter a valid Email")
+            return false
+        }
+        
+        if password.count < 5 {
+            print("Enter a valid Password")
+            return false
+        }
+        
+        return true
+    }
+    
+    func authenticateUser(email: String, password: String) {
+        Auth.auth().signIn(withEmail: email, password:password){
+            authResult, error in
+            
+            if let err = error {
+                print(err.localizedDescription)
+                return
+            }
+            
+            if let result = authResult {
+                print("User Email : \(result.user.email ?? "No found")")
+            }
+        }
+    }
+}
---
+++ CafeNIBM/Model/EntityModel.swift
@@ -0,0 +1,30 @@
+//
+//  EntityModel.swift
+//  CafeNIBM
+//
+//  Created by Imasha on 4/13/21.
+//
+
+import Foundation
+
+struct User{
+    var userName: String
+    var userEmail: String
+    var userPassword: String
+    var userPhone: String
+    
+}
+
+struct FoodItem{
+    var foodName: String
+    var foodDescription: String
+    var foodPrice: Double
+    var discount: Int
+}
+
+//class User1{
+//var userName: String
+//var userEmail: String
+//var userPassword: String
+//var userPhone: String
+//}
